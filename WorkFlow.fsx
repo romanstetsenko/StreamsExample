f62a7b79e0dbf583c6f48751398006ad74640814
Rowan <stetsenkorp@gmail.com>
2019-01-08 00:13:43 +0100

    Update WorkFlow.fsx

--- WorkFlow.fsx
+++ WorkFlow.fsx
@@ -1,160 +1,172 @@
 open System.Threading
 open System
 
+type ValuableData =
+    | Json of string
+    | DbRequest of int
+    | DataSet of int * string array
+    | Processed of int * string
+    | Reduce of int * string array
+    | Map of string
 
+type Guard = ValuableData -> Result<ValuableData, string>
 
+type DbReader = ValuableData -> Result<ValuableData, string>
 
-type JsonA =
-    { id : string }
+type BussinesLogic = ValuableData -> Result<ValuableData, string>
 
-type DbRequestA =
-    { id : int }
-
-type RawDataA =
-    { id : int
-      values : string array }
-
-type ProcessedDataA =
-    { id : int
-      aggregatedValue : string }
-
-type Guard = JsonA -> Result<DbRequestA, string>
-
-type DbReader = DbRequestA -> Result<RawDataA, string>
-
-type BussinesLogic =  RawDataA -> Result<ProcessedDataA, string>
-
-type GenericWorkFlow = JsonA -> DbRequestA -> RawDataA -> ProcessedDataA
-
+type GenericWorkFlow = ValuableData -> ValuableData -> ValuableData -> ValuableData
 
 module Implementations =
+    let randomlyWait (x : int) =
+        let delta = (float x) * 0.05 |> int
+        Random().Next(x - delta, x + delta) |> Thread.Sleep
+    
     let private tryParse s =
         match Int32.TryParse s with
         | true, i -> Ok i
         | false, _ -> Error "Cannot parse int."
     
-    let guard (arg : JsonA) : Result<DbRequestA, string> =
-        arg.id
+    let guard (Json someId) : Result<ValuableData, string> =
+        someId
         |> tryParse
-        |> Result.map (fun x -> { id = x })
+        |> Result.map DbRequest
     
     let private readEachValue x =
+        randomlyWait 200
         printfn "Reading db value: %i " x
-        Thread.Sleep(1000)
+        randomlyWait 800
         printfn "Db value read: %i " x
         string x
     
     let private readEachValueAsync x =
         async { 
+            randomlyWait 200
             printfn "Reading db value: %i " x
-            Thread.Sleep(1000)
+            randomlyWait 800
             printfn "Db value read: %i " x
             return (string x)
         }
     
-    let dbReaderSequential (arg : DbRequestA) : Result<RawDataA, string> =
+    let dbReaderSequential (DbRequest id) : Result<ValuableData, string> =
         try 
             if DateTime.Now.Millisecond % 3 = 0 then failwith "TIMEOUT."
-            [| 1..arg.id |]
+            [| 1..id |]
             |> Array.map readEachValue
-            |> fun xs -> 
-                { id = arg.id
-                  values = xs }
+            |> fun values -> DataSet(id, values)
             |> Ok
         with e -> Error e.Message
     
-    let dbReaderParallel (arg : DbRequestA) : Result<RawDataA, string> =
+    let dbReaderParallel (DbRequest id) : Result<ValuableData, string> =
         try 
             if DateTime.Now.Millisecond % 3 = 0 then failwith "TIMEOUT."
-            [| 1..arg.id |]
+            [| 1..id |]
             |> Array.map readEachValueAsync
             |> (Async.Parallel >> Async.RunSynchronously)
-            |> fun xs -> 
-                { id = arg.id
-                  values = xs }
+            |> fun xs -> DataSet(id, xs)
             |> Ok
         with e -> Error e.Message
     
-    let bussinesLogic (arg : RawDataA) : Result<ProcessedDataA, string> =
+    let bussinesLogic (DataSet(id, data)) : Result<ValuableData, string> =
         let processEachValue x =
+            randomlyWait 200
             printfn "Processing item: %s " x
-            Thread.Sleep(3000)
+            randomlyWait 2800
             printfn "Item processed: %s " x
             x
         
         let aggregatedValue =
-            arg.values
+            data
             |> Array.map processEachValue
             |> String.concat ", "
         
-        Ok { id = arg.id; aggregatedValue = aggregatedValue }
+        Ok(Processed(id, aggregatedValue))
     
-    let bussinesLogicMap x =
+    let bussinesLogicMap (Map x) =
+        randomlyWait 200
         printfn "Processing item: %s " x
-        Thread.Sleep(3000)
+        randomlyWait 2800
         printfn "Item processed: %s " x
         x
     
-    let bussinesLogicReduce id xs : Result<ProcessedDataA, string> =
+    let bussinesLogicReduce (Reduce(id, xs)) : Result<ValuableData, string> =
         let aggregatedValue = xs |> String.concat ", "
-        Ok { id = id; aggregatedValue = aggregatedValue }
+        Ok(Processed(id, aggregatedValue))
 
+module Posibilities =
+    type Handler = Handler of (ValuableData -> Result<ValuableData, string>)
+    
+    let someHandlers =
+        [| Handler Implementations.bussinesLogic
+           Handler Implementations.dbReaderSequential
+           Handler Implementations.bussinesLogic |]
+    
+    let loggedSomeHandlers =
+        someHandlers
+        |> Array.map (fun (Handler handler) -> 
+               let wrapper valuableData =
+                   printf "LOGING INPUT: %A" valuableData
+                   let result = handler valuableData
+                   printf "LOGING OUTPUT: %A" result
+                   result //return 
+               Handler wrapper)
 
-let wofkflowBuilder (guard: Guard) (dbReader: DbReader)  (bussinesLogic: BussinesLogic) = 
+let wofkflowBuilder (guard : Guard) (dbReader : DbReader) 
+    (bussinesLogic : BussinesLogic) =
     //let result1 = guard input
     //let result2 = Result.bind dbReader result1
     //let result3 = Result.bind bussinesLogic result2
     //return result3
-    guard >> Result.bind dbReader >> Result.bind bussinesLogic
+    guard
+    >> Result.bind dbReader
+    >> Result.bind bussinesLogic
 
 #time "on"
 
 //--------------------
 let runWf1Seq input =
-    let wf = 
-        wofkflowBuilder 
-            Implementations.guard 
-            Implementations.dbReaderSequential 
+    printfn "//// Sequential ////"
+    let wf =
+        wofkflowBuilder Implementations.guard Implementations.dbReaderSequential 
             Implementations.bussinesLogic
     wf input
 
-runWf1Seq ({ id = "3" } : JsonA)
-runWf1Seq ({ id = "!@#$%^" } : JsonA)
+runWf1Seq (Json "3")
+runWf1Seq (Json "!@#$%^")
 
 //--------------------
-
 let runWf1Par input =
-    let wf = 
-        wofkflowBuilder 
-            Implementations.guard 
-            Implementations.dbReaderParallel 
+    printfn "//// Sequential + dbReaderParallel ////"
+    let wf =
+        wofkflowBuilder Implementations.guard Implementations.dbReaderParallel 
             Implementations.bussinesLogic
     wf input
 
-runWf1Par ({ id = "3" } : JsonA)
-runWf1Par ({ id = "!@#$%^" } : JsonA)
+runWf1Par (Json "3")
+runWf1Par (Json "!@#$%^")
 
 //--------------------
-
-let forkBL (input : RawDataA) =
-    input.values
+let forkBL (DataSet(id, values)) =
+    values
     |> Array.map 
-           (fun x -> async { return (Implementations.bussinesLogicMap x) })
+           (fun value -> 
+           async { return (Implementations.bussinesLogicMap (Map value)) })
     |> (Async.Parallel
         >> Async.Catch
         >> Async.RunSynchronously)
     |> function 
     | Choice2Of2 e -> Error e.Message
-    | Choice1Of2 xs -> Ok xs
-    |> Result.bind (Implementations.bussinesLogicReduce input.id)
+    | Choice1Of2 values -> Ok values
+    |> Result.bind 
+           (fun values -> 
+           Implementations.bussinesLogicReduce (Reduce(id, values)))
 
 let runWf1Complicated input =
-    let wf = 
-        wofkflowBuilder 
-            Implementations.guard 
-            Implementations.dbReaderParallel 
+    printfn "//// Parallel ////"
+    let wf =
+        wofkflowBuilder Implementations.guard Implementations.dbReaderParallel 
             forkBL
     wf input
 
-runWf1Complicated ({ id = "3" } : JsonA)
-runWf1Complicated ({ id = "!@#$%^" } : JsonA)
+runWf1Complicated (Json "10")
+runWf1Complicated (Json "!@#$%^")
